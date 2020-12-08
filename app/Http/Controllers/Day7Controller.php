b59337e0a50fc2a314ad824a6b8a23e1f7ab539b
mcfleew <you@example.comfleury.timbe@gmail.com>
2020-12-08 20:56:26 +0100

    advent of code (aoc) 2020 - day 7 part 2

--- app/Http/Controllers/Day7Controller.php
+++ app/Http/Controllers/Day7Controller.php
@@ -10,17 +10,17 @@ class Day7Controller extends Controller
 {
     public function __construct(InputRepository $inputRepository) {
         $this->inputRepository = $inputRepository;
-        $this->bagToSearch = 'shiny gold bags';
+        $this->bagGoldAndShiny = 'shiny gold bags';
     }
 
     public function part1() {
         $this->rules = $this->inputRepository->getLuggagesRules();
         $rulesThatContainShinyBag = $this->rules->filter(function($rule) {
-            if ($rule->bagContainer === $this->bagToSearch) return false;
-            $this->fillMatrixWithAllBagsThatMustContain($rule->bagContainer);
+            if ($rule->bagContainer === $this->bagGoldAndShiny) return false;
+            $this->getTraceOfBagsContainedBy($rule->bagContainer);
 
-            return $this->bagsMatrix->search(function ($finalRuleSum, $finalRuleKey) {
-                return Str::contains($finalRuleKey, $this->bagToSearch);
+            return $this->bagsTrace->first(function ($bagContainer) {
+                return $bagContainer === $this->bagGoldAndShiny;
             });
         });
         return $rulesThatContainShinyBag->count();
@@ -28,38 +28,63 @@ class Day7Controller extends Controller
 
     public function part2() {
         $this->rules = $this->inputRepository->getLuggagesRules();
-        return 'To Do';
+        return $this->getSumOfBagsContainedBy($this->bagGoldAndShiny);
     }
 
-    public function fillMatrixWithAllBagsThatMustContain($bagToFind) {
+    public function getBagsContainedBy($bagToFind, $traceGoldAndShiny = true) {
         $this->bagsToFind = collect([$bagToFind]);
-        $this->bagsMatrix = collect([$bagToFind => 0]);
+        $this->bagsBuffer = collect();
 
-        while (!$this->bagsToFind->isEmpty()) {
-            $this->bagsBuffer = collect();
-            $this->bagsToForget = collect();
+        while (!$this->bagsToFind->isEmpty() && !($traceGoldAndShiny
+            && $this->bagsToFind->contains($this->bagGoldAndShiny))) {
+            $this->bagsToFind->each(function ($bagToFind) {
+                $rule = $this->rules->first(function ($rule) use ($bagToFind) {
+                    return $rule->bagContainer === $bagToFind;
+                });
 
-            $this->rules->each(function ($rule) {
-                $bagContainer = $rule->bagContainer;
-                if ($this->bagsToFind->contains($bagContainer)) {
-                    $this->bagsToFind->forget($bagContainer);
+                $this->bagsBuffer = $this->bagsBuffer->concat($rule->bagsContained->keys());
 
-                    $rule->bagsContained->each(function ($bagNumber, $bagName) use ($bagContainer) {
-                        $keyContainer = $this->bagsMatrix->search(function ($sum, $key) use ($bagContainer) {
-                            return Str::endsWith($key, $bagContainer);
-                        });
-                        $bagNumber += $this->bagsMatrix->get($keyContainer);
-                        $newKeyContainer = $keyContainer.'.'.$bagName;
-
-                        $this->bagsMatrix->prepend($bagNumber, $newKeyContainer);
-                        $this->bagsToForget->prepend($keyContainer);
-                    });
-                    $this->bagsBuffer = $this->bagsBuffer->concat($rule->bagsContained->keys());
-                }
+                if (isset($this->bagsTrace)) $this->traceAllBagsInsideBigBag($bagToFind, $rule);
+                if (isset($this->calcString)) $this->countAllBagsInsideBigBag($bagToFind, $rule);
             });
-            $this->bagsMatrix = $this->bagsMatrix->except($this->bagsToForget);
             $this->bagsToFind = $this->bagsBuffer;
-            $this->bagsToForget = collect();
+            $this->bagsBuffer = collect();
         }
+    }
+
+    public function traceAllBagsInsideBigBag($bagToFind, $rule) {
+        if ($rule->bagsContained->has($this->bagGoldAndShiny)) {
+            $this->bagsTrace->prepend($this->bagGoldAndShiny);
+        }
+    }
+
+    public function countAllBagsInsideBigBag($bagToFind, $rule) {
+        if (!$rule->bagsContained->isEmpty()) {
+            $this->calcString = preg_replace_array('/'.$bagToFind.':'.$bagToFind.'/', array_fill(0, 10,
+                $bagToFind.':'.$rule->bagsContained->keys()->implode(' + '.$bagToFind.':')), $this->calcString);
+                
+            $rule->bagsContained->each(function ($bagCount, $bagContained) use ($bagToFind) {
+                $this->calcString = preg_replace_array('/'.$bagToFind.':'.$bagContained.'/', array_fill(0, 10,
+                '('.$bagCount.' + '.$bagCount.' * ('.$bagContained.':'.$bagContained.'))'), $this->calcString);
+            });
+        } else {                    
+            $this->calcString = preg_replace_array(
+                '/\s\+\s\d\s\*\s\(('.$bagToFind.':'.$bagToFind.'|\s\+\s)\)/',
+                array_fill(0, 10, ''), $this->calcString);
+        }
+    }
+
+    public function getTraceOfBagsContainedBy($bagToFind) {
+        $this->bagsTrace = collect([$bagToFind]);
+        $this->getBagsContainedBy($bagToFind);
+    }
+
+    public function getSumOfBagsContainedBy($bagToFind) {
+        $this->calcString = $bagToFind.':'.$bagToFind;
+        $this->getBagsContainedBy($bagToFind, false);
+
+        $calc = $this->calcString;
+        eval('$calc = '.$calc.';'); 
+        return $calc;
     }
 }
