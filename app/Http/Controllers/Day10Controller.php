c3f0000cc4b712aa6a3f0258f7c8f0e818c6d55d
mcfleew <you@example.comfleury.timbe@gmail.com>
2020-12-10 18:46:03 +0100

    advent of code (aoc) 2020 - day 10 part 2

--- app/Http/Controllers/Day10Controller.php
+++ app/Http/Controllers/Day10Controller.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Repositories\InputRepository;
 
+use Illuminate\Support\Str;
+
 class Day10Controller extends Controller
 {
     public function __construct(InputRepository $inputRepository) {
         $this->inputRepository = $inputRepository;
         $this->diffJoltsMin = 1;
         $this->diffJoltsMax = 3;
+        $this->alternatives = collect();
     }
 
     public function part1() {
@@ -18,8 +21,8 @@ class Day10Controller extends Controller
         $this->diffCounter = $this->getDifferencesCounter();
 
         foreach($this->builtInAdapterRange as $builtInAdapter) {
-            $joltagePossibilities = $this->getJoltagePossibilities($builtInAdapter);
-            $adaptersConcerned = $this->adapters->whereBetween('joltage', $joltagePossibilities);
+            $joltageAlternatives = $this->getJoltageAlternatives($builtInAdapter);
+            $adaptersConcerned = $this->adapters->whereBetween('joltage', $joltageAlternatives);
 
             $this->countDifferences($builtInAdapter, $adaptersConcerned);
         }
@@ -28,6 +31,9 @@ class Day10Controller extends Controller
 
     public function part2() {
         $this->adapters = $this->inputRepository->getJoltageAdapters();
+        $answerPart1 = $this->part1();
+        $alternativeWays = $this->countAlternatives();
+        return $alternativeWays->count();
     }
 
     public function getBuiltInAdapter() {
@@ -41,7 +47,7 @@ class Day10Controller extends Controller
         return array_fill($this->diffJoltsMin, $this->diffJoltsMax, 0);
     }
 
-    public function getJoltagePossibilities($builtInAdapter) {
+    public function getJoltageAlternatives($builtInAdapter) {
         return [
             $builtInAdapter['joltage'] + $this->diffJoltsMin,
             $builtInAdapter['joltage'] + $this->diffJoltsMax
@@ -50,9 +56,42 @@ class Day10Controller extends Controller
 
     public function countDifferences($builtInAdapter, $adaptersConcerned) {
         if ($adaptersConcerned->isNotEmpty()) {
+            if ($adaptersConcerned->count() > 1) {
+                $this->alternatives->push($adaptersConcerned->toArray());
+            }
             $adapterConcerned = $adaptersConcerned->shift();
             $diffKey = $adapterConcerned['joltage'] - $builtInAdapter['joltage'];
             $this->diffCounter[$diffKey]++;
         }
+    }
+
+    public function countAlternatives() {
+        $alternativesWays = collect([':0']);
+
+        foreach($this->alternatives as $alternativesAdapters) {
+            $waysToAdd = collect();
+            $waysToRem = collect();
+
+            foreach($alternativesAdapters as $alternativeAdapter) {
+                $waysConcerned = $alternativesWays->filter(function ($alternativeWay) use ($alternativeAdapter) {
+                    return Str::afterLast($alternativeWay,':') < $alternativeAdapter['joltage'];
+                });
+                
+                if ($waysConcerned->isNotEmpty()) {
+                    $wayToAdd = $waysConcerned->crossJoin([':'.$alternativeAdapter['joltage']]);
+                    $wayToAdd->transform(function($splittedWay) {
+                        return implode($splittedWay);
+                    });
+                    $waysToAdd = $waysToAdd->merge($wayToAdd);
+                    $waysToRem = $waysToRem->merge($waysConcerned);
+                }
+            }
+
+            $alternativesWays = $alternativesWays->merge($waysToAdd);
+            $alternativesWays = $alternativesWays->reject(function ($way) use ($waysToRem) {
+                return $waysToRem->contains($way);
+            });
+        }
+        return $alternativesWays->sort()->values();
     }
 }
